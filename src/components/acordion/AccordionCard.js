dbedad725d7c31f94f9d88ca63a3becc1553fa3b
farnai <f.iordanishvili@gmail.com>
2024-06-03 03:37:53 +0400

    Add accordion

--- src/components/acordion/AccordionCard.js
+++ src/components/acordion/AccordionCard.js
@@ -1,16 +1,9 @@
 import styles from './AcordionCard.module.css'
 import {useState} from "react";
 import AccordionItem from './AccordionItem';
+import contentAccordion from './accordionData.json';
 
-const contentAccordion = [
-        {
-            itemNumber: 1,
-            title: "Accordion title",
-            content: "111"
-        },
-        
-    ]
-;
+
 function AccordionCard (){
     const [activeAccordionNumber, setActiveAccordionNumber] = useState (null);
 
@@ -29,41 +22,14 @@ function AccordionCard (){
         return <div className={styles.cardContainer}>
             {contentAccordion.map ((item) => (
                 <AccordionItem
+                    key={item.itemNumber}
                     activeAccordionNumber={activeAccordionNumber}
                     handleClick={handleClick}
-                    itemNumber={1}
-                    title="Accordion titile1"
-                    content="111"
-                />)
-            )},a
-            <AccordionItem
-                activeAccordionNumber={activeAccordionNumber}
-                handleClick={handleClick}
-                itemNumber={1}
-                title="Accordion titile1"
-                content="111"
-            />
-            <AccordionItem
-                activeAccordionNumber={activeAccordionNumber}
-                handleClick={handleClick}
-                itemNumber={2}
-                title="Accordion titile2"
-                content="222"
-            />
-            <AccordionItem
-                activeAccordionNumber={activeAccordionNumber}
-                handleClick={handleClick}
-                itemNumber={3}
-                title="Accordion title3"
-                content="333"
-            />
-            <AccordionItem
-                activeAccordionNumber={activeAccordionNumber}
-                handleClick={handleClick}
-                itemNumber={4}
-                title="Accordion titil4"
-                content="4"
-            />
+                    itemNumber={item.itemNumber}
+                    title={item.title}
+                    content={item.content}
+                />
+            ))},
         </div>
 
 }
